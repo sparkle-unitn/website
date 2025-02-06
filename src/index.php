d67123805eb9151c8c7c6c2f1b94eda95cef32b3
Kalsifer <kalsifer742@gmail.com>
2025-02-06 19:06:17 +0100

    starting to integrate php and database

--- src/index.php
+++ src/index.php
@@ -1,3 +1,17 @@
+<?php
+$servername = "localhost";
+$username = "sparkle";
+$password = "admin";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?> 
+
 <!DOCTYPE html>
 <head>
     <meta charset="UTF-8" />
@@ -64,15 +78,31 @@
                     </svg>
                 </a>
             </div>
-            <ol class="relative border-s border-gray-200 border-gray-700">                  
+            <ol class="relative border-s border-gray-200 border-gray-700">
+                <?php
+                    $sql = "SELECT * FROM blogposts";
+                    $result = $conn->query($sql);
+                    
+                    if ($result->num_rows > 0) {
+                      // output data of each row
+                      while($row = $result->fetch_assoc()) {
+                        echo "id: " . $row["id"]. "<br>";
+                      }
+                    } else {
+                      echo "0 results";
+                    }
+                ?>
                 <li class="mb-10 ms-4">
                     <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white border-gray-900 bg-gray-700"></div>
                     <time class="mb-1 text-sm font-normal leading-none text-gray-400 text-gray-500">February 2022</time>
                     <h3 class="text-lg font-semibold text-gray-900 text-white">Application UI code in Tailwind CSS</h3>
                     <p class="mb-4 text-base font-normal text-gray-500 text-gray-400">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
-                    <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 bg-gray-800 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700 focus:ring-gray-700">Learn more <svg class="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg></a>
+                    <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 bg-gray-800 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700 focus:ring-gray-700">
+                        Learn more
+                        <svg class="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                    </a>
                 </li>
                 <li class="mb-10 ms-4">
                     <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white border-gray-900 bg-gray-700"></div>
